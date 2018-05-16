6b1fbce9fe2093d1f4689206c25b92b52af29663
FoxClaire <36248666+FoxClaire@users.noreply.github.com>
2018-05-16 14:02:37 -0400

    Add files via upload

---
+++ hw3redo/hw3redo.cpp
@@ -0,0 +1,72 @@
+*/ClaireFox
+*hw3
+
+#include <iostream>
+
+// #include <utility>
+
+
+
+int main()
+
+{
+
+  using namespace std;
+
+int a[] = {4,9,2,1,3,4,7,8,5,0,6}; //this is the array
+
+
+
+for (int j = 0; j < 11; j++) {
+
+
+
+    for (int i = 0; i < 10; i++)
+
+{
+
+  // cout <<"i: " << i<<endl;
+
+//   cout<< a[i] << endl;
+
+
+
+  if (a[i+1] <  a[i])
+
+  //index inside of a
+
+   {
+
+int x= a[i];
+
+a[i] = a[i+1];
+
+a[i+1] = x;
+
+//this passes the variable
+
+  // cout <<"i: " << i<<endl;
+
+  // cout <<"i+1: " << i+1 <<endl;
+
+  }
+
+}
+
+}
+
+for (int k = 0; k < 11; k++)
+
+{
+
+cout<< a[k] << endl;
+
+}
+
+
+
+return 0;
+
+
+
+}
